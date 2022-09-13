1279ac490db7b364f952641cfb694445d18e1a4e
M_b_96 <maciejbielik1996@gmail.com>
2022-09-13 19:01:20 +0200

    Adding Position Class And Block Class

---
+++ Tetris/Block.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        //Two dimention position array to show four states of tiles rotation
+        protected abstract Position[][] Tiles { get; }
+
+        //StartOffset which decides where the block will spawn on the grid
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        // Method that returns grid position where the block is.
+        // Method loops over the tiles positions in current positionState and adds rowOffset and then columnOffset
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach(Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        public void Move (int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+    }
+}
---
+++ Tetris/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    //Position - representing the position of a specific block in the game grid
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
